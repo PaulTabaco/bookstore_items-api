aff6a215095922f63de3fb46e3bd46a2627a705d
Paul Tabaco <rabochiy.delta@gmail.com>
2022-12-16 19:57:13 +0200

    - errors changed according last bookstore_utils ver;

--- services/items_service.go
+++ services/items_service.go
@@ -13,17 +13,17 @@ var (
 )
 
 type itemsServiceInterface interface {
-	Create(items.Item) (*items.Item, *rest_errors.RestErr)
-	Get(string) (*items.Item, *rest_errors.RestErr)
+	Create(items.Item) (*items.Item, rest_errors.RestErr)
+	Get(string) (*items.Item, rest_errors.RestErr)
 }
 
 type itemsService struct {
 }
 
-func (s *itemsService) Create(items.Item) (*items.Item, *rest_errors.RestErr) {
+func (s *itemsService) Create(items.Item) (*items.Item, rest_errors.RestErr) {
 	return nil, rest_errors.NewRestError("should be implemented", http.StatusNotImplemented, "not implemented", nil)
 }
 
-func (s *itemsService) Get(string) (*items.Item, *rest_errors.RestErr) {
+func (s *itemsService) Get(string) (*items.Item, rest_errors.RestErr) {
 	return nil, rest_errors.NewRestError("should be implemented", http.StatusNotImplemented, "not implemented", nil)
 }
